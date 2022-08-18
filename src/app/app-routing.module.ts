ee45ef9676806701fa7be49bce158fd32ea4864b
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-08-17 21:20:43 -0300

    Criado base para tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './access/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './access/components/login/login.component';
+import { CreateAccountComponent } from './access/components/create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
