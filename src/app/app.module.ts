c69118e4536304789653726ed7e3bdb029cc1e70
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-08-23 07:36:59 -0300

    Alterado header para expandir os detalhes da conta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './access/components/login/login.component';
 import { CreateAccountComponent } from './access/components/create-account/create-account.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CreateAccountComponent } from './access/components/create-account/creat
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
