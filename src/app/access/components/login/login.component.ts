0ebe5376448a373ac190b0cb285d6d36f97595dd
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-09-02 08:26:14 -0300

    Adicionado redirecionamento para tela inicial

--- src/app/access/components/login/login.component.ts
+++ src/app/access/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { AccessService } from './../../services/access/access.service';
 import { LoginDTO } from './../../models/loginDTO.model';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,17 @@ export class LoginComponent implements OnInit {
 
   login: LoginDTO = new LoginDTO();
 
-  constructor(private accessService: AccessService) { }
+  constructor(private accessService: AccessService, private router : Router) { }
 
   ngOnInit(): void {
- 
+
   }
 
   sendLogin() {
-    this.accessService.loginCustomer(this.login).subscribe();
+    this.accessService.loginCustomer(this.login).subscribe(res => {
+      AccessService.setUser(res.data);
+      this.router.navigate(['']);
+    });
   }
 
 }
