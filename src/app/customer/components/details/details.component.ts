43075dba431341944393b23d5fb871ca51b19e96
danilo jesus souza <dannilojesusouza@gmail.com>
2022-09-04 11:44:04 -0300

    edit account

--- src/app/customer/components/details/details.component.ts
+++ src/app/customer/components/details/details.component.ts
@@ -3,6 +3,7 @@ import { CustomerDTO } from './../../../access/models/custumerDTO.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { CustumerService } from 'src/app/access/services/custumer/custumer.service';
 
 @Component({
   selector: 'app-details',
@@ -13,9 +14,11 @@ export class DetailsComponent implements OnInit {
 
   formAccount: FormGroup = new FormGroup({});
   customer: CustomerDTO = new CustomerDTO();
-  edit: boolean = false;
+  edit: boolean = true;
+  editPassword: boolean = false;
 
-  constructor(private formbuilder: FormBuilder, public datePipe: DatePipe) { }
+  constructor(private formbuilder: FormBuilder, public datePipe: DatePipe,
+              private custumerService: CustumerService) { }
 
   ngOnInit(): void {
     this.createForm();
@@ -23,7 +26,8 @@ export class DetailsComponent implements OnInit {
 
   createForm() {
     this.formAccount = this.formbuilder.group({
-      name: [this.customer.name],
+      //this.customer.name
+      name: ['xxXXX'],
       phone: [this.customer.phoneNumber],
       password: [this.customer.password],
       birthDate: [this.datePipe.transform(this.customer.birthDate, 'dd/MM/yyyy')],
@@ -37,4 +41,11 @@ export class DetailsComponent implements OnInit {
     })
   }
 
+  editForm() {
+    this.edit = false;
+  }
+
+  editPassowrd() {
+    this.editPassword = true;
+  }
 }
