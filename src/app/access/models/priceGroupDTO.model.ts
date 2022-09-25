3c93d52cfa92129ac5419c3765d9eaf60720191e
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-09-25 19:21:51 -0300

    Corrigido crud de price group

--- src/app/access/models/priceGroupDTO.model.ts
+++ src/app/access/models/priceGroupDTO.model.ts
@@ -1,13 +1,13 @@
 export class PriceGroupDTO {
-  public Id: string;
-  public Name: string;
-  public Description: string;
-  public ProfitMargin: number;
+  public id: string;
+  public name: string;
+  public description: string;
+  public profitMargin: number;
 
   constructor() {
-    this.Id = '00000000-0000-0000-0000-000000000000';
-    this.Name = '';
-    this.Description = '';
-    this.ProfitMargin = 0;
+    this.id = '00000000-0000-0000-0000-000000000000';
+    this.name = '';
+    this.description = '';
+    this.profitMargin = 0;
   }
 }
--- src/app/adm/config/price-categories/price-categories.component.ts
+++ src/app/adm/config/price-categories/price-categories.component.ts
@@ -1,7 +1,7 @@
 import { PriceGroupService } from './../../../access/services/pricre/priceGroup.service';
 import { PriceGroupDTO } from './../../../access/models/priceGroupDTO.model';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-price-categories',
@@ -12,7 +12,12 @@ export class PriceCategoriesComponent implements OnInit {
 
   public priceCategory: PriceGroupDTO = new PriceGroupDTO();
   public formPriceCategoryList: FormGroup[] = [];
-  public formPriceCategory: FormGroup = new FormGroup({});
+  public formPriceCategory: FormGroup = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+    description: new FormControl(),
+    profitMargin: new FormControl()
+  });
   public isPriceCategoryCreate = false;
   public isPriceCategoryEdit = false;
 
@@ -25,6 +30,7 @@ export class PriceCategoriesComponent implements OnInit {
   getAllPriceCategory() {
     this.priceGroupService.getPriceGroups().subscribe(res => {
       if(res.message === "Price Group Found") {
+        this.formPriceCategoryList = [];
         res.data.forEach(priceCategory => {
           this.formPriceCategoryList.push(this.createPriceCategoryForm(priceCategory));
         })
@@ -33,10 +39,10 @@ export class PriceCategoriesComponent implements OnInit {
   }
 
   createPriceCategory(priceCategoryForm: FormGroup) {
-    this.priceCategory.Id = priceCategoryForm.controls['id'].value;
-    this.priceCategory.Name = priceCategoryForm.controls['name'].value;
-    this.priceCategory.Description = priceCategoryForm.controls['description'].value;
-    this.priceCategory.ProfitMargin = priceCategoryForm.controls['priceCategory'].value;
+    this.priceCategory.id = priceCategoryForm.controls['id'].value;
+    this.priceCategory.name = priceCategoryForm.controls['name'].value;
+    this.priceCategory.description = priceCategoryForm.controls['description'].value;
+    this.priceCategory.profitMargin = priceCategoryForm.controls['profitMargin'].value;
 
     this.priceGroupService.createPriceGroup(this.priceCategory).subscribe(res => {
       if(res.message === "Price Group Created") {
@@ -49,19 +55,19 @@ export class PriceCategoriesComponent implements OnInit {
 
   createPriceCategoryForm(priceCategory: PriceGroupDTO) {
     return this.formBuilder.group({
-      id: [priceCategory.Id],
-      name: [priceCategory.Name],
-      description: [priceCategory.Description],
-      profitMargin: [priceCategory.ProfitMargin],
+      id: [priceCategory.id],
+      name: [priceCategory.name],
+      description: [priceCategory.description],
+      profitMargin: [priceCategory.profitMargin],
       priceCategoryEdit: [this.isPriceCategoryEdit]
     })
   }
 
   updateCategory(priceCategoryForm: FormGroup) {
-    this.priceCategory.Id = priceCategoryForm.controls['id'].value;
-    this.priceCategory.Name = priceCategoryForm.controls['name'].value;
-    this.priceCategory.Description = priceCategoryForm.controls['description'].value;
-    this.priceCategory.ProfitMargin = priceCategoryForm.controls['priceCategory'].value;
+    this.priceCategory.id = priceCategoryForm.controls['id'].value;
+    this.priceCategory.name = priceCategoryForm.controls['name'].value;
+    this.priceCategory.description = priceCategoryForm.controls['description'].value;
+    this.priceCategory.profitMargin = priceCategoryForm.controls['profitMargin'].value;
 
     this.priceGroupService.updatePriceGroup(this.priceCategory).subscribe(res => {
       if(res.message === "Price Group Updated") {
@@ -83,9 +89,9 @@ export class PriceCategoriesComponent implements OnInit {
   }
 
   deletePriceCategory(priceCategoryForm: FormGroup) {
-    this.priceCategory.Id = priceCategoryForm.controls['id'].value;
+    this.priceCategory.id = priceCategoryForm.controls['id'].value;
 
-    this.priceGroupService.deletePriceGroup(this.priceCategory.Id).subscribe(res => {
+    this.priceGroupService.deletePriceGroup(this.priceCategory.id).subscribe(res => {
       if(res.message === "Price Group Deleted") {
         this.getAllPriceCategory();
       }
