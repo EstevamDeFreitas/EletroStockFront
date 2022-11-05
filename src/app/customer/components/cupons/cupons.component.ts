f5c9e413fc4d956e7fd4c972774f4887f1171d0c
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-11-05 12:56:38 -0300

    adicionado lsitagem de cupons

--- src/app/customer/components/cupons/cupons.component.ts
+++ src/app/customer/components/cupons/cupons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CouponCustomerDTO } from 'src/app/access/models/cupomDTO';
+import { CouponService } from 'src/app/access/services/coupon/coupon.service';
 
 @Component({
   selector: 'app-cupons',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuponsComponent implements OnInit {
 
-  constructor() { }
+  public cupons : CouponCustomerDTO[] = [];
+
+  constructor(private couponService :CouponService) { }
 
   ngOnInit(): void {
+    this.getCustomerCoupons();
+  }
+
+  getCustomerCoupons(){
+    this.couponService.getCustomerCoupons().subscribe(res => {
+      this.cupons = res.data;
+    })
   }
 
 }
